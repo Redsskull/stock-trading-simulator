1b4ce1d05b118cf1473384465474f015d56a1825
Redsskull <ivry91@gmail.com>
2025-09-04 16:48:46 +0200

    I forgot guinicorn again!

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# Install dependencies
 pip install -r requirements.txt
+
+# Initialize database
 python -c "
 from app import app, db
 with app.app_context():
-    db.create_all()
+    from flask_migrate import upgrade
+    upgrade()
     print('Database tables created successfully!')
 "
